489d135b128e375fd7cd8aeb119e3cd04bfef001
anagha157 <69710957+anagha157@users.noreply.github.com>
2022-11-12 16:53:29 +0530

    update navbar.html

--- Project Deliverables/Skill_Job_Recommendation_System/app.py
+++ Project Deliverables/Skill_Job_Recommendation_System/app.py
@@ -1,4 +1,8 @@
-from flask import Flask
+from flask import Flask,render_template
 
 
 app = Flask(__name__)
+
+@app.route('/register')
+def register():
+    return render_template('Registeration.html')
