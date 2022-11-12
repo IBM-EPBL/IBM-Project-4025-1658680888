c3a5874c11f95e2fb9d6c0ed928bd7b2d4064078
anagha157 <69710957+anagha157@users.noreply.github.com>
2022-11-12 22:26:13 +0530

    update db

--- Project Deliverables/Skill_Job_Recommendation_System/app.py
+++ Project Deliverables/Skill_Job_Recommendation_System/app.py
@@ -1,4 +1,7 @@
 from flask import Flask,render_template
+import ibm_db
+
+conn = ibm_db.connect("DATABASE=bludb;HOSTNAME=b1bc1829-6f45-4cd4-bef4-10cf081900bf.c1ogj3sd0tgtu0lqde00.databases.appdomain.cloud;PORT=32304;SECURITY=SSL;SSLServerCertificate=DigiCertGlobalRootCA.crt;UID=khc44923;PWD=VMdkxmMxV1Z30kOH",'','')
 
 
 app = Flask(__name__)
@@ -6,3 +9,7 @@ app = Flask(__name__)
 @app.route('/register')
 def register():
     return render_template('Registeration.html')
+
+@app.route('/insert', methods = ['POST', 'GET'])
+def insert():
+    return
